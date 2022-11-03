da2ed1171fa2286511a243f1675b805acee39460
RaulPineda12 <zonico200@gmail.com>
2022-11-03 15:20:34 -0600

    init

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-require('./conexion');
-
 const express= require('express')
 const cors= require('cors')
-const port=(process.env.port);
+const port=(process.env.port || 3001);
 
 //express
 const app= express();
@@ -27,3 +25,27 @@ app.listen(app.get('port'),(err)=>{
         console.log("servidor corriendo en el puerto: "+port);
     }
 })
+
+var conexion = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'raul',
+        password: '@Tiburon1279',
+        server: 'roomreservationsystem-db-server2.database.windows.net',
+        database: 'RoomReservationSystem-db',
+        options: {
+            encrypt: true // Use this if you're on Windows Azure
+        }
+    };
+
+
+
+conexion.connect(config,(err)=>{
+    if(err){
+        console.log("error en la conexion a la base de datos");
+        console.log(err);
+    }else{
+        console.log("Conexion exitosa a la base de datos");
+    }
+});
